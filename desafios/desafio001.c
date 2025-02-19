8910a1dea5d20dbd6e00bfa161d612681d1c4574
Isac B. Matos <91500212+IsacBM@users.noreply.github.com>
2025-02-19 14:43:10 -0300

    Add files via upload

---
+++ desafios/desafio001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int A, B;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &A);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &B);
+
+    printf("X = %d", A+B);
+
+    return 1;
+}
